4b5a7ebe503d31ce98deff7c014c5bad6d6625b5
dandindan <431bog431@gmail.com>
2022-08-02 04:16:42 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 # https://www.youtube.com/watch?v=JP7ITIXGpHk&list=PLhQjrBD2T3817j24-GogXmWqO5Q5vYy0V&index=2
 
 print("Hello World")
+name = input("What is your name? ")
+print("Hello " + name)
+print("The length of your name is: " + str(len(name)))
+print("Your name in uppercase is: " + name.upper())
+print("Your name in lowercase is: " + name.lower())
