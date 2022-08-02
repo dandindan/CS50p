b2e10971e20883460856937e0f434fdf1d90b672
dandindan <431bog431@gmail.com>
2022-08-02 04:43:49 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@
 print("Hello World")
 name = input("What is your name? ")
 print("Hello " + name)
+# remove the whitespace from the input
+name = name.strip()
 print("The length of your name is: " + str(len(name)))
 print("Your name in uppercase is: " + name.upper())
 print("Your name in lowercase is: " + name.lower())
