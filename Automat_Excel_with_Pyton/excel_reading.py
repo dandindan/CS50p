d6392182fce8cdf8129011a0e7d8ef002462b3b6
dandindan <431bog431@gmail.com>
2022-09-08 22:25:34 +0300

    update

--- Automat_Excel_with_Pyton/excel_reading.py
+++ Automat_Excel_with_Pyton/excel_reading.py
@@ -4,3 +4,12 @@ wb = openpyxl.load_workbook('store.xlsx')
 
 print(wb.sheetnames)
 
+#create new sheet named Maoz
+# wb.create_sheet(title='Maoz') 
+print(wb.worksheets)
+
+for sheet in wb:
+    print(sheet.title)
+
+# save worksheet
+#wb.save('store.xlsx')
