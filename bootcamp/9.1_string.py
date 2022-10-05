099c2e2091bd4ad1ccf5d16b2dcc17bf836ed94a
dandindan <431bog431@gmail.com>
2022-10-05 08:05:55 +0300

    added the string files

---
+++ bootcamp/9.1_string.py
@@ -0,0 +1,17 @@
+sample_string = "This is a large string"
+
+#concatinate
+print('green '+'bus')
+
+#repeat the string
+print('hi'*5)
+
+#convert a int a string 
+num_str = str(64)
+print(num_str)
+print(type(num_str))
+x=0
+#cycle through a string
+for i in sample_string:
+    print(x,'=',i) 
+    x+=1
