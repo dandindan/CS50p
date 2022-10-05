74a7d1ffa51f923b21730210f9f24803e5326f0c
dandindan <431bog431@gmail.com>
2022-10-05 20:59:32 +0300

    Create 6.1_primre.py

---
+++ bootcamp/6.1_primre.py
@@ -0,0 +1,7 @@
+for n in range(2, 100):
+    for x in range(2, int(n**0.5)+1):
+        if n % x == 0:  # if n is divisible by x, it means it's not a prime number.
+            print(f"{n} equals {x} * {n//x}")
+            break
+    else:  # if n was not divisible by any x, it means it is a prime number.
+        print(f"{n} is a prime number.")
