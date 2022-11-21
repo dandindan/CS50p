810c53a11384fb55b02e4a3c9506177d9e862f05
dandindan <431bog431@gmail.com>
2022-11-21 17:20:04 +0200

    h1

--- dash/app.py
+++ dash/app.py
@@ -6,12 +6,12 @@ app.layout = html.Div([
     html.Div(
         className="app-header",
         children=[
-            html.Div('Plotly Dash', className="app-header--title")
+            html.Div('Title', className="app-header--title")
         ]
     ),
     html.Div(
         children=html.Div([
-            html.H5('Overview'),
+            html.H1('Overview'),
             html.Div('''
                 This is an example of a simple Dash app with
                 local, customized CSS.
